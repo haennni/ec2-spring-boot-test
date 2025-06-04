6b7146b2449eb4cf9dcf6a99de818820c5ee0091
haennni <ggowater112@naver.com>
2025-06-04 21:10:18 +0900

    feat: RDS 테스트 파일 업로드

--- src/main/java/com/example/ec2springbootsample/controller/HomeController.java
+++ src/main/java/com/example/ec2springbootsample/controller/HomeController.java
@@ -1,18 +1,35 @@
 package com.example.ec2springbootsample.controller;
 
+import com.example.ec2springbootsample.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.example.ec2springbootsample.entity.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    private final UserRepository userRepository;
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/about")
+    @GetMapping("/user-form")
     public String about() {
         return "about";
+    }
+
+    @PostMapping("/users")
+    public String saveUser(@RequestParam String name, @RequestParam String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        userRepository.save(user);
+        return "redirect:/user-form";
     }
 }
